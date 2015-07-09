1b18fa30d7233992395190024faba400d7549945
makafis <vinnyalfieri@gmail.com>
2015-07-09 15:59:31 -0400

    complete

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,18 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+  def self.my_all
+    Classification.distinct
+  end
+
+  def self.longest_boat
+    Boat.order(length: :desc).limit(1)
+  end
+
+  def self.longest
+
+    # includes(:boats).order(boats.length: :desc).limit(1)
+    joins(:boats).where("boat_id IN (?)", self.longest_boat.pluck(:id))
+  end
+
 end
